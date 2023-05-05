be4d6ac9612b9dad26666b52894738b1ea0560de
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-05-06 00:02:01 +0100

    update script.js

--- script.js
+++ script.js
@@ -9,10 +9,6 @@ const dayInput = document.querySelector('#day');
 const monthInput = document.querySelector('#month');
 const yearInput = document.querySelector('#year');
 
-const dayOutput = document.querySelector('.h1');
-const monthOutput = document.querySelector('.h2');
-const yearOutput = document.querySelector('.h3');
-
 
 //Numbers input only
 dayInput.addEventListener('input', (e) => {
@@ -31,6 +27,7 @@ yearInput.addEventListener('input', (e) => {
     e.target.value = numbersOnly;
 });
 
+
 //Submission of input
 submitBtn.addEventListener('click', function(e){
     e.preventDefault();
@@ -39,11 +36,9 @@ submitBtn.addEventListener('click', function(e){
     const monthError = document.querySelector('.month-span');
     const yearError = document.querySelector('.year-span');
 
-    let dayNum = dayInput.value;
-    let monthNum = monthInput.value;
-    let yearNum = yearInput.value;
 
     //conditions for empty input fields and wrong days, months and years
+    let dayNum = dayInput.value;
     if (!dayInput.value){
         inputs[0].classList.add('error');
         dayError.innerText = 'This field is required';
@@ -58,6 +53,7 @@ submitBtn.addEventListener('click', function(e){
         label[0].style.color = '';
     }
 
+    let monthNum = monthInput.value;
     if (!monthInput.value){
         inputs[1].classList.add('error');
         monthError.innerText = 'This field is required';
@@ -72,11 +68,14 @@ submitBtn.addEventListener('click', function(e){
         label[1].style.color = '';
     }
 
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let yearNum = yearInput.value;
     if (!yearInput.value){
         inputs[2].classList.add('error');
         yearError.innerText = 'This field is required';
         label[2].style.color = 'hsl(0, 100%, 67%)';
-    } else if (yearNum > 2022){
+    } else if (yearNum >= currentYear){
         inputs[2].classList.add('error');
         yearError.innerText = 'Must be in the past';
         label[2].style.color = 'hsl(0, 100%, 67%)';
@@ -86,5 +85,31 @@ submitBtn.addEventListener('click', function(e){
         label[2].style.color = '';
     }
 
+    // let birthday = `${dayNum}/${monthNum}/${yearNum}`;
+    // // function ageCalculator(birthday){
+    //     let birthDate = new Date(birthday);
+    //     let today = new Date();
     
+    //     let yearAge = today.getFullYear() - birthDate.getFullYear();
+    //     let monthAge = today.getMonth() - birthDate.getMonth();
+    //     let dayAge = today.getDate() - birthDate.getDate();
+    
+    //     if (monthAge < 0 || (monthAge === 0 && dayAge < 0)){
+    //         yearAge-=1;
+    //         if (monthAge === 0) {
+    //             monthAge = 11;
+    //         } else {
+    //             monthAge = 12 + monthAge;
+    //         }
+    //         days = 30 + days;
+    //     }
+    //     let yearOutput = document.querySelector('.h1');
+    //     let monthOutput = document.querySelector('.h2');
+    //     let dayOutput = document.querySelector('.h3');
+    
+    //     yearOutput.innerHTML = `${yearAge}`;
+    //     monthOutput.innerHTML = monthAge;
+    //     dayOutput.innerHTML = dayAge;
+    // }
+    // ageCalculator(birthday);
 })
